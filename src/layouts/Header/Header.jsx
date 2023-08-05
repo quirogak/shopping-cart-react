3821b69aecab838c51347800d2dde516f54f6418
quirogak <quirogakill@gmail.com>
2023-08-05 18:53:06 -0500

    Added basic Header component structure.

--- src/layouts/Header/Header.jsx
+++ src/layouts/Header/Header.jsx
@@ -1,9 +1,25 @@
-import "../styles/header.css";
-import NavBar from "../NavBar/NavBar";
-const Header = (props) => {
+import { Link } from "react-router-dom";
+import "/src/styles/header.css";
+import NavBar from "../NavBar/NavBar"
+
+const Header = () => {
   return (
     <header>
-      <h1>{props.logo}</h1>
+      <Link to={"/"} className="logo">
+        Shopping Cart
+      </Link>
+      <Link to={"/shop"} className="header-item">
+        Products
+      </Link>
+      <Link to={"/shop/clothing"} className="header-item">
+        Clothing
+      </Link>
+      <Link to={"/shop/electronics"} className="header-item">
+        Electronics
+      </Link>
+      <Link to={"/shop/jewelry"} className="header-item">
+        Jewelry
+      </Link>
       <NavBar></NavBar>
     </header>
   );
